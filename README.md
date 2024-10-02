2706715f0b519b22e6fb0bef6560a898c170e737
GOOD-AN <53306377+GOOD-AN@users.noreply.github.com>
2024-10-02 20:25:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Web Crypto Param
+
+This project is used for encrypted parameter generation for individual site requests.
+
+## Disclaimer
+
+This project was originally intended for easy archiving by the project owner.
+
+It is intended for study and research purposes only and may not be used for any commercial or illegal purposes.
+
+If others need to use the functions provided by this project, the user is responsible for all legal liabilities brought about.
+
+By using the contents of this repository, you agree to all the terms and conditions of this disclaimer. If you do not accept the above disclaimer, please stop using this program immediately.
---
+++ douyin/ac_signature/ac_signature.py
@@ -0,0 +1,31 @@
+import subprocess
+
+import httpx
+
+
+client = httpx.Client(follow_redirects=False)
+main_url = "https://v.douyin.com/iktn4pDJ/"
+main_headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/129.0.0.0 Safari/537.36"
+}
+client.headers.update(main_headers)
+while True:
+    main_response = client.get(main_url)
+    if main_response.status_code not in [200, 302]:
+        break
+    if '__ac_nonce' in main_response.cookies.keys():
+        break
+    main_url = main_response.headers["Location"]
+
+ac_url = main_url[8:]
+ac_nonce = main_response.cookies["__ac_nonce"]
+ac_signature = subprocess.run(['node', './ac_signature.js', ac_url, ac_nonce, main_headers["user-agent"]],
+                              capture_output=True)
+ac_signature = ac_signature.stdout.decode('utf-8').strip()
+client.cookies.update({
+    "__ac_referer": "__ac_blank",
+    "__ac_signature": ac_signature
+})
+main_response = client.get(main_url)
+print(main_response.cookies)
